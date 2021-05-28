9f24a932cb716be977fdf567ce8bd6b5b796c950
mati <53273027+MrFunkee@users.noreply.github.com>
2021-05-28 00:12:48 -0400

    Un par de validaciones en registro.html
    puse validaciones en los atributos de los formularios en el html (minlength y maxlength) para mín y máx de caracteres.
    Agregué validación del rut con regex para que use determinado formato 12345678-9 incluyendo la k en el dígito verificador.
    Me falta validar lo demás

--- js/js_registro.js
+++ js/js_registro.js
@@ -1,42 +1,65 @@
+//La validación del mínimo de caracteres se hace en el archivo html con "minlength". Usado en el campo de contraseña
+//lo mismo con el máximo, con el atributo "maxlength"
+
+//darle constantes a los elementos de formulario
 const formu = document.getElementById("formulario");
 const nombre = document.getElementById("nombre");
 const rut = document.getElementById("rut");
+const contra1 = document.getElementById("password");
+const contra2 = document.getElementById("validate-password");
+
+//regex para validar rut
+const regexRut = new RegExp('([0-9]{8})+[-]+[0-9/k]{1}');
 
 
 formu.addEventListener('submit', (e) =>{
     e.preventDefault();
 
-    revisarInputs();
+    revisarCampos();
 });
 
-function revisarInputs() {
-    //conseguir valores de los inputs:
+function revisarCampos() {
+    //conseguir valores de los Campo:
     const nombreValor = nombre.value.trim();
     const rutValor = rut.value.trim();
     
     if(nombreValor === null , nombreValor.length == 0 ) {
-        //evento de error
-        mostrarError(nombre,'El campo está vacío')
+        mostrarError(nombre,'El campo está vacío');
+
     } else {
-        //evento de exito
+        afirmarCorrecto(nombre)
     }
 
     if(rutValor === null , rutValor.length == 0 ) {
-        //evento de error
-        mostrarError(rut,'El campo está vacío')
-    } else {
-        //evento de exito
+        //evento de error campo vacio
+        mostrarError(rut,'El campo está vacío');
+    } else if(!regexRut.test(rutValor)) {
+        //viendo si se adapta al regex que dice 8 numeros, un guión y un número o "k"
+        mostrarError(rut,'el rut tiene que ser escrito en el formato 12345678-9');
     }
+    else {
+        afirmarCorrecto(rut)
+    }
+
     
 }
+//cambia el elemento small que es invisible y está debajo de los campos del formulario para mostrar el error dependiendo de la situación
+function mostrarError (campo, mensaje) {
 
-
-
-function mostrarError (input, mensaje) {
-    const divPadre = input.parentElement;
+    const divPadre = campo.parentElement;
     const small = divPadre.querySelector("small");
 
     small.innerText = mensaje;
 
-    divPadre.classList.add("error")
+    divPadre.classList.add("error");
+}
+
+//lo mismo que mostrarError() pero con el mensaje fijo de un ✔ verde
+function afirmarCorrecto (campo) {
+    const divPadre = campo.parentElement;
+    const small = divPadre.querySelector("small");
+
+    small.innerText = '✔';
+
+    divPadre.classList.add("correcto");
 }
