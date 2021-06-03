d861c4a790c0b779dacdc1889e76fd798b8a6542
lSeezl <s.lopezmunoz@duocuc.cl>
2021-06-03 02:35:02 -0400

    tabla user 0.3

--- js/usermiapi.js
+++ js/usermiapi.js
@@ -4,15 +4,91 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.open ('GET','https://randomuser.me/api/',true);
 xmlhttp.send();
 
+
+
+
+
 xmlhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
         var data = JSON.parse(this.responseText);
 
-       var txtNombre = data.results.name.first;
-       var txtEmail = data.results.email;
+        for(var i = 0; i < data.results.length;i++){
+            var txtNombre = data.results[i].name.first+' '+data.results[i].name.last;
+            var txtEmail = data.results[i].email;
+            var txtEdad = data.results[i].registered.age;
+            var txtPais = data.results[i].location.country;
+            var txtcell = data.results[i].cell;
+            
+        }
+       
 
     }
     document.getElementById("nombre").innerHTML = txtNombre;
-
+    document.getElementById("contacto").innerHTML = '-Metodo de Contacto  (celular) ' +txtcell+' '+'(Email) '+txtEmail;
+    document.getElementById("informacion").innerHTML = '-Tiene '+txtEdad+' '+'años y vive en '+txtPais;
+  
 }
 });
+
+$(document).ready(function(){
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open ('GET','https://randomuser.me/api/',true);
+    xmlhttp.send();
+    
+    
+     
+    
+    xmlhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var data = JSON.parse(this.responseText);
+    
+            for(var i = 0; i < data.results.length;i++){
+                var txtNombre = data.results[i].name.first+' '+data.results[i].name.last;
+                var txtEmail = data.results[i].email;
+                var txtEdad = data.results[i].registered.age;
+                var txtPais = data.results[i].location.country;
+                var txtcell = data.results[i].cell;
+                
+            }
+           
+    
+        }
+        document.getElementById("nombre2").innerHTML = txtNombre;
+        document.getElementById("contacto2").innerHTML = '-Metodo de Contacto  (celular) ' +txtcell+' '+'(Email) '+txtEmail;
+        document.getElementById("informacion2").innerHTML = '-Tiene '+txtEdad+' '+'años y vive en '+txtPais;
+      
+    }
+    });
+
+$(document).ready(function(){
+
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open ('GET','https://randomuser.me/api/',true);
+        xmlhttp.send();
+        
+  
+        xmlhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                var data = JSON.parse(this.responseText);
+        
+                for(var i = 0; i < data.results.length;i++){
+                    var txtNombre = data.results[i].name.first+' '+data.results[i].name.last;
+                    var txtEmail = data.results[i].email;
+                    var txtEdad = data.results[i].registered.age;
+                    var txtPais = data.results[i].location.country;
+                    var txtcell = data.results[i].cell;
+                    
+                }
+               
+        
+            }
+            document.getElementById("nombre3").innerHTML = txtNombre;
+            document.getElementById("contacto3").innerHTML = '-Metodo de Contacto  (celular) ' +txtcell+' '+'(Email) '+txtEmail;
+            document.getElementById("informacion3").innerHTML = '-Tiene '+txtEdad+' '+'años y vive en '+txtPais;
+          
+        }
+        });
+        
+
+       
