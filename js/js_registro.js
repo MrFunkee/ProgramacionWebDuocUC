356d574829fa7c78ee39b911bd45c2896ae17d98
MrFunkee <53273027+MrFunkee@users.noreply.github.com>
2021-06-02 21:39:21 -0400

    todas las validaciones listas
    Solo falta agregar la función para dar los caracteres faltantes cuando agregas rut que pide la rúbrica

--- js/js_registro.js
+++ js/js_registro.js
@@ -9,10 +9,13 @@ const email = document.getElementById("email");
 const fecha = document.getElementById("fecha_nac");
 const contra1 = document.getElementById("password");
 const contra2 = document.getElementById("validate-password");
+const telef = document.getElementById("telefono");
+const coment = document.getElementById("comentario");
 
 //regex para validar rut
 const regexRut = new RegExp('([0-9]{8})+[-]+[0-9/k]{1}');
 const regexEmail = new RegExp("[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)");
+const regexTelef = new RegExp("[0-9]{9}")
 
 formu.addEventListener('submit', (e) =>{
     e.preventDefault();
@@ -28,6 +31,8 @@ function revisarCampos() {
     const fechaValor = fecha.value;
     const contra1Valor = contra1.value;
     const contra2Valor = contra2.value;
+    const telefValor = telef.value.replace(/ /g, "");
+    const comentValor = coment.value.trim();
 
         //para trabajar fechas y ver si es mayor de 18
     let cumple = new Date(fechaValor);
@@ -90,7 +95,22 @@ function revisarCampos() {
         afirmarCorrecto(contra2)
     }
 
-        
+        //telefono
+    if(telefValor === null , telefValor.length == 0 ) {
+        mostrarError(telef,'El campo está vacío');
+    } else if(!regexTelef.test(telefValor) || telefValor.length>9) {
+        //viendo si se adapta al regex de mail
+        mostrarError(telef,'Por favor, ingrese solo 9 números');
+    } else {
+        afirmarCorrecto(telef)
+    }
+
+        //Comentario
+    if(comentValor === null , comentValor.length == 0 ) {
+        mostrarError(coment,'Deja una opinión por favor!');
+    } else {
+        afirmarCorrecto(coment)
+    }
     
 }
 //cambia el elemento small que es invisible y está debajo de los campos del formulario para mostrar el error dependiendo de la situación
