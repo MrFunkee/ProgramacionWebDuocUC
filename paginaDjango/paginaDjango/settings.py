b4a1e0b63908e454c9089fc0243a52576fb09892
MrFunkee <53273027+MrFunkee@users.noreply.github.com>
2021-07-07 23:08:32 -0400

    api y acerca_de

--- paginaDjango/paginaDjango/settings.py
+++ paginaDjango/paginaDjango/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
     'rest_vehiculo',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- paginaDjango/rest_vehiculo/serializers.py
+++ paginaDjango/rest_vehiculo/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from core.models import Producto
+from core.models import Producto, Empleado
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = ['idProducto','descripcion','precio', 'cantidad', 'imagen']
+
+class EmpleadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Empleado
+        fields = ['idEmpleado','nombreEmpleado','descripcion', 'imagen']
