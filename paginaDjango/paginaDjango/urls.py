9da7b48714edf932df31c425a57a7b01cb0114b5
MrFunkee <53273027+MrFunkee@users.noreply.github.com>
2021-07-07 20:28:05 -0400

    instalada la api 'lista_productos'
    para que funcione en su pc tienen que hacer pip install djangorestframework

--- paginaDjango/paginaDjango/urls.py
+++ paginaDjango/paginaDjango/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
+    path('api/', include('rest_vehiculo.urls')),
 ]
 
 if settings.DEBUG:
